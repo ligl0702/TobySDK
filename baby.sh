98ad045d89645090cfd8a7ba2cfe90c4f01359b7
wkdaily <jinn0702@126.com>
2023-08-30 21:45:19 +0800

    Update baby.sh

--- baby.sh
+++ baby.sh
@@ -1,11 +1,41 @@
 #!/bin/sh
+##add some note
+echo "一键安装三合一开始..."
+uci set system.@system[0].description='wkopenwrt'
+uci set system.@system[0].notes='插件的使用方法详见:
+https://didiboy0702.gitbook.io/ruan-lu-you-shi-yong-zhi-nan/passwall'
+uci commit system
 
-# 获取 model name 的值
-model_name=$(cat /proc/cpuinfo | grep "model name" | head -n 1 | cut -d ':' -f 2 | sed -e 's/^[ \t]*//')
+echo "设置自定义域名劫持(主机名映射)..."
+#add hostname for google tv
+uci add dhcp domain
+uci set dhcp.@domain[-1].name='time.android.com'
+uci set dhcp.@domain[-1].ip='203.107.6.88'
+uci commit dhcp
 
-# 检查 model name 的值是否是 "Intel(R) N100"
-if [ "$model_name" = "Intel(R) N100" ]; then
-    echo "n100"
-else
-    echo "other cpu"
-fi
+##install kxsw tools
+echo "下载三大科学插件..."
+cd /tmp
+wget https://raw.githubusercontent.com/AUK9527/Are-u-ok/main/x86/all//OpenClash_x86_update.run
+wget https://raw.githubusercontent.com/AUK9527/Are-u-ok/main/x86/all/PassWall_x86_update.run
+wget https://raw.githubusercontent.com/AUK9527/Are-u-ok/main/x86/all/SSR-Plus_x86_update.run
+echo "安装三大科学插件..."
+sh OpenClash_x86_update.run
+sh PassWall_x86_update.run
+sh SSR-Plus_x86_update.run
+
+## add keeflys.com for emotn store
+echo "添加emotn store域名"
+sed -i "s/keeflys.com//g" "/usr/share/passwall/rules/proxy_host"
+echo -n "keeflys.com" | tee -a /usr/share/passwall/rules/proxy_host
+sed -i "s/keeflys.com//g" "/etc/ssrplus/black.list"
+echo -n "keeflys.com" | tee -a /etc/ssrplus/black.list
+
+## install luci-app-poweroff and luci-app-ddnsto
+echo "安装关机等必备插件"
+is-opkg install 'app-meta-poweroff'
+is-opkg install 'app-meta-ddnsto'
+is-opkg install 'app-meta-systools'
+echo "全部安装完毕"
+
+
