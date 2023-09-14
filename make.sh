b0911ddfeeb3c2f04d2e39b9de3e56b5bd4a15d4
wkdaily <jinn0702@126.com>
2023-09-14 17:54:27 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,5 +2,4 @@
 #curl -sL https://ghproxy.com/https://raw.githubusercontent.com/ligl0702/TobySDK/master/make.sh | sh
 opkg update
 opkg install bash
-cd /tmp
-wget -O 2500.sh https://raw.githubusercontent.com/ligl0702/TobySDK/master/2500.sh && chmod +x 2500.sh &&./2500.sh
+wget -O /tmp/2500.sh https://ghproxy.com/https://raw.githubusercontent.com/ligl0702/TobySDK/master/2500.sh && chmod +x /tmp/2500.sh && /tmp/2500.sh
